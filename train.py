52f1e7cecd06dd2592c7c9baa773bee927fc2f36
ThankGod Egbe <egbethankgodjnr@gmail.com>
2023-07-29 06:09:16 +0000

    updates

--- train.py
+++ train.py
@@ -1,10 +1,10 @@
 #@title ##Input the data
 # Write short explanation @markdown
 import pandas as pd
-from welltest.utility import *
+from utility import *
 import io
 
 file_name = '/content/Well 4SS x Inhouse Data.xlsx'
 welltest_df = get_data(file_name) #ok 'welltest/6LS Well Inhouse Data.xlsx'
 welltest_df_stnd = standardize_column_names(welltest_df)
-welltest_df_stnd
+print(welltest_df_stnd.head(3))
